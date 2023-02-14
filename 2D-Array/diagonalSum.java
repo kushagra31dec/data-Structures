a6641e3776038aee569a3bb2b9b1a235c3bd19d7
kushagra31dec <6kushagrarawat@gmail.com>
2023-02-15 00:43:16 +0530

    prints diagonalSum

--- 2D-Array/diagonalSum.java
+++ 2D-Array/diagonalSum.java
@@ -11,11 +11,17 @@ public class diagonalSum {
         System.out.println(sum);
     }
     public static void main(String[] args) {
-        int matrix[][] = {
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,10,11,12},
-            {13,14,15,16}};
-            printDiagonalSum(matrix);
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns of the matrix");
+        int m = scn.nextInt();
+
+        int matrix[][] = new int[m][m];
+        System.out.println("Enter the elements of the matrix");
+        for (int i = 0; i < m; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = scn.nextInt();
+                }
+        }
+        printDiagonalSum(matrix);
     }
 }
